4ddef28ecfad61ec6a434fdfd8409892d62a7a87
Tiziano Müller <tm@dev-zero.ch>
2011-11-09 23:43:28 +0100

    the animation converted to pure vtk (without mayavi)

--- lorenz/lorenz.py
+++ lorenz/lorenz.py
@@ -9,7 +9,8 @@
 
 from scipy.integrate import ode
 from numpy import array, linspace, append
-from mayavi import mlab
+import vtk
+import random
 
 def lorenz(t, c, sigma, r, b):
     return array([sigma*(c[1]-c[0]), c[0]*(r - c[2])-c[1], c[0]*c[1] - b*c[2]])
@@ -19,30 +20,64 @@ params = (10., 28., 8./3.)
 t = linspace(0., 100., 5000)
 
 o = ode(lorenz)
-values = array([c0])
-s = mlab.plot3d(values[:,0], values[:,1], values[:,2], color=(1,1,1))
-ms = s.mlab_source
 
-@mlab.animate(delay=10, ui=False)
-def generateLorenz():
-    global values, ms
+points = vtk.vtkPoints()
+points.InsertPoint(0, c0)
+lines = vtk.vtkCellArray()
+lines.InsertNextCell(1) # number of points
+lines.InsertCellPoint(0)
 
-    f = mlab.gcf()
-    mlab.view(distance=200)
+track = vtk.vtkPolyData()
+track.SetPoints(points)
+track.SetLines(lines)
 
-    while True:
-        values = array([c0])
-        o.set_initial_value(c0, 0.).set_f_params(params[0], params[1], params[2])
-        ms.reset(color=(0.5,0.5,0.5))
-        i = 0
-        print "generating a new one"
-        while o.successful() and o.t < 100.:
-            o.integrate(o.t+0.02)
-            values = append(values, [o.y], axis=0)
-            if i % 10 == 0:
-                ms.reset(x=values[:,0], y=values[:,1], z=values[:,2])
-                yield
-            i += 1
+mapper = vtk.vtkPolyDataMapper()
+mapper.SetInput(track)
 
-g = generateLorenz()
-mlab.show()
+actor = vtk.vtkActor()
+actor.SetMapper(mapper)
+actor.GetProperty().SetColor(random.random(), random.random(), random.random())
+
+ren = vtk.vtkRenderer()
+renWin = vtk.vtkRenderWindow()
+renWin.AddRenderer(ren)
+iren = vtk.vtkRenderWindowInteractor()
+iren.SetRenderWindow(renWin)
+
+# Add the actors to the renderer, set the background and size
+ren.AddActor(actor)
+
+ren.SetBackground(0, 0, 0)
+#renWin.SetSize(500, 500)
+renWin.SetFullScreen(1)
+
+iren.Initialize()
+iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
+renWin.Render()
+
+o.set_initial_value(c0, 0.).set_f_params(params[0], params[1], params[2])
+i = 1
+
+def update(obj, event):
+    global i
+
+    if o.successful() and o.t < 100.:
+        o.integrate(o.t+0.02)
+        points.InsertPoint(i, o.y)
+        points.Modified()
+
+        lines.InsertNextCell(2)
+        lines.InsertCellPoint(i-1)
+        lines.InsertCellPoint(i)
+        lines.Modified()
+
+        iren = obj
+        ren.ResetCamera()
+        iren.GetRenderWindow().Render()
+    
+        i += 1
+
+iren.AddObserver('TimerEvent', update)
+iren.CreateRepeatingTimer(1);
+
+iren.Start()
