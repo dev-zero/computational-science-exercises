edc61833461388c2d04c0c62b54aaa2cb22e1a03
Tiziano Müller <tm@dev-zero.ch>
2011-11-10 19:52:27 +0100

    reorganized source code again and added cli arguments and help

--- lorenz/lorenz.py
+++ lorenz/lorenz.py
@@ -7,16 +7,15 @@
 #
 #
 
-from scipy.integrate import ode
 from numpy import array
-import vtk
-import random
 
 class Lorenz:
     ''' Formulate the problem, capsulate the ODE solver
         and provide access functions for points, initial conditions
         and parameters '''
     def __init__(self):
+        from scipy.integrate import ode
+
         # parameters
         self.sigma = 10.
         self.r = 28.
@@ -63,69 +62,113 @@ class Lorenz:
         return self.i
 
 def randomLorenzParameters():
-    return (random.random()*20, random.random()*50, random.random()*10)
+    from random import random
+    return (random()*20, random()*50, random()*10)
 
-l = Lorenz()
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description="draw Lorenz plots in screensaver style")
+    parser.add_argument('-f', '--fullscreen', dest='fullscreen', action='store_const', const=True, default=False, help="go fullscreen")
+    parser.add_argument('-s', '--steps', dest='steps', metavar='N', action='store', type=int, default=1000, help="do N simulation steps before generating a new set of parameters and restart drawing")
 
-points = vtk.vtkPoints()
-lines = vtk.vtkCellArray()
+    args = parser.parse_args()
 
-track = vtk.vtkPolyData()
-track.SetPoints(points)
-track.SetLines(lines)
+    # create the Lorenz object
+    l = Lorenz()
 
-mapper = vtk.vtkPolyDataMapper()
-mapper.SetInput(track)
+    import vtk
+    import random
 
-actor = vtk.vtkActor()
-actor.SetMapper(mapper)
-actor.GetProperty().SetColor(random.random(), random.random(), random.random())
+    # define the points and lines arrays
+    points = vtk.vtkPoints()
+    lines = vtk.vtkCellArray()
 
-ren = vtk.vtkRenderer()
-renWin = vtk.vtkRenderWindow()
-renWin.AddRenderer(ren)
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renWin)
+    # dataset which represents the line objects
+    track = vtk.vtkPolyData()
+    track.SetPoints(points)
+    track.SetLines(lines)
 
-# Add the actors to the renderer, set the background and size
-ren.AddActor(actor)
+    # map the line objects to graphic primitives
+    mapper = vtk.vtkPolyDataMapper()
+    mapper.SetInput(track)
 
-ren.SetBackground(0, 0, 0)
-renWin.SetFullScreen(1)
+    # represents the graphic primitives in the scene
+    actor = vtk.vtkActor()
+    actor.SetMapper(mapper)
 
-iren.Initialize()
-iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
-renWin.Render()
+    # create a rendered and a window
+    ren = vtk.vtkRenderer()
+    renWin = vtk.vtkRenderWindow()
+    renWin.AddRenderer(ren)
 
-def update(obj, event):
-    if l.getIteration() < 1000 and l.getIteration() > 0:
-        points.InsertPoint(l.getIteration(), l.getValue())
-        points.Modified()
+    # create an interactor for the window
+    iren = vtk.vtkRenderWindowInteractor()
+    iren.SetRenderWindow(renWin)
 
-        lines.InsertNextCell(2)
-        lines.InsertCellPoint(l.getIteration()-1)
-        lines.InsertCellPoint(l.getIteration())
-        lines.Modified()
+    # Add the actors to the renderer, set the background and size
+    ren.AddActor(actor)
+    ren.SetBackground(0, 0, 0)
 
+    # make it fullscreen
+    if args.fullscreen:
+        renWin.SetFullScreen(1)
     else:
-        sigma, r, b = randomLorenzParameters()
-        l.setParams(sigma, r, b)
-        points.Reset()
-        points.InsertPoint(l.getIteration(), l.getValue())
-        points.Modified()
-        lines.Reset()
-        lines.InsertNextCell(1)
-        lines.InsertCellPoint(0)
-        lines.Modified()
+        renWin.SetSize(400, 400)
+    
+    # initialize the interactor and set the control style to trackball
+    iren.Initialize()
+    iren.SetInteractorStyle(vtk.vtkInteractorStyleTrackballCamera())
 
-    if not l.eval():
-        import sys.exit
-        exit(1)
+    # start rendering
+    renWin.Render()
 
-    iren = obj
-    ren.ResetCamera()
-    iren.GetRenderWindow().Render()
+    def update(obj, event):
+        if l.getIteration() < args.steps and l.getIteration() > 0:
+            # add the new point
+            points.InsertPoint(l.getIteration(), l.getValue())
+            points.Modified()
+    
+            # and update the lines accordingly
+            lines.InsertNextCell(2)
+            lines.InsertCellPoint(l.getIteration()-1)
+            lines.InsertCellPoint(l.getIteration())
+            lines.Modified()
+    
+        else:
+            sigma, r, b = randomLorenzParameters()
+            print "starting with new parameters (sigma, r, b) = ", sigma, r, b
+            l.setParams(sigma, r, b)
+    
+            # reset the points array, does not free the space
+            # and therefore makes sure we don't always allocate/free memory
+            points.Reset()
+            points.InsertPoint(l.getIteration(), l.getValue())
+            points.Modified()
 
-iren.AddObserver('TimerEvent', update)
-iren.CreateRepeatingTimer(1);
-iren.Start()
+            # ... and do the ame for the lines
+            lines.Reset()
+            lines.InsertNextCell(1)
+            lines.InsertCellPoint(0)
+            lines.Modified()
+
+            # let the actor paint in a new random color
+            actor.GetProperty().SetColor(random.random(), random.random(), random.random())
+
+        if not l.eval():
+            import sys.exit
+            exit(1)
+
+        # resets the camera (only "zoom out") to include the complete line
+        ren.ResetCamera()
+
+        # and rotate the view a little (we could have used Yaw instead to rotate the scene instead of the camera)
+        ren.GetActiveCamera().Azimuth(0.5)
+
+        # the obj is the RenderWindowInteractor since the callback was registered for it
+        # render again
+        obj.GetRenderWindow().Render()
+
+    iren.AddObserver('TimerEvent', update)
+    iren.CreateRepeatingTimer(1);
+    iren.Start()
+
