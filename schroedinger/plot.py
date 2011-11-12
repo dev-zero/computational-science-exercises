4b7f45ae4156fab9691b1b0b6b81015d043aca14
Tiziano Müller <tm@dev-zero.ch>
2011-11-12 19:07:05 +0100

    added first version of my python 1D-schroedinger simulation using matplotlib to do the animation

---
+++ schroedinger/plot.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011 Tiziano Müller <tm@dev-zero.ch>
+#
+#
+#
+#
+
+from schroedinger import Schroedinger1D
+
+import gobject
+import matplotlib
+matplotlib.use('GTKAgg')
+
+from pylab import plot, show, figure
+from numpy import abs, real, imag, vectorize
+
+s1 = Schroedinger1D(256, L=20)
+s2 = Schroedinger1D(256, L=20)
+
+def doubleWellPotential(x):
+    sigma = 0.04
+    pos = 2.
+
+    if (x < pos) or (x > pos+sigma):
+        return 0.
+    return 10.
+
+s2.setPotential(vectorize(doubleWellPotential))
+
+fig = figure()
+ax = fig.add_subplot(211)
+ax.set_title("Press 'space' to start the simulation")
+ax.set_ylim(-1, 1)
+
+s1_l_amp,  = plot(s1.x(), abs(s1.f()), color='black')
+s1_l_real, = plot(s1.x(), s1.f().real, color='red')
+s1_l_imag, = plot(s1.x(), s1.f().imag, color='blue')
+s1_l_pot   = plot(s1.x(), s1.V(), color='green')
+
+ax = fig.add_subplot(212)
+ax.set_ylim(-1, 1)
+
+s2_l_amp,  = plot(s2.x(), abs(s2.f()), color='black')
+s2_l_real, = plot(s2.x(), s2.f().real, color='red')
+s2_l_imag, = plot(s2.x(), s2.f().imag, color='blue')
+s2_l_pot   = plot(s2.x(), s2.V(), color='green')
+
+runSimulation = False
+
+def update():
+    s1.evolve(0.01)
+    s2.evolve(0.01)
+
+    s1_l_amp.set_ydata(abs(s1.f()))
+    s1_l_real.set_ydata(real(s1.f()))
+    s1_l_imag.set_ydata(imag(s1.f()))
+    s2_l_amp.set_ydata(abs(s2.f()))
+    s2_l_real.set_ydata(real(s2.f()))
+    s2_l_imag.set_ydata(imag(s2.f()))
+    fig.canvas.draw_idle()
+    return runSimulation
+
+def onpress(event):
+    global runSimulation
+    if event.key==' ':
+        if runSimulation:
+            runSimulation = False
+        else:
+            runSimulation = True
+            gobject.idle_add(update)
+
+fig.canvas.mpl_connect('key_press_event', onpress)
+show()
+
---
+++ schroedinger/schroedinger.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2011 Tiziano Müller <tm@dev-zero.ch>
+#
+#
+#
+#
+
+from numpy import zeros, exp, concatenate, arange, pi, abs, sqrt, array
+from scipy import fft, ifft
+
+def harmonicPotential(x):
+    return 0.5*x**2
+
+
+class Schroedinger1D:
+    def __init__(self, N=256, L=10):
+        self._N = N
+        self._L = L
+        self._F = zeros(N)
+
+        self._dx = 2.*L/N
+        self._dk = 2.*pi / (N*self._dx)
+
+        self._x = (arange(N) - N/2) * self._dx
+        k = (arange(N) - N/2)
+        self._k = concatenate((k[self._N//2:], k[0:self._N//2])) * self._dk
+        self._f = exp(-0.5*self._x**2 + 2.*self._x*1j)
+        self._V = harmonicPotential
+
+    def evolve(self, dt = 0.001):
+        def spatialEvolve(f, x):
+            return f*exp(-0.5 * self._V(x) * dt * 1j)
+        def momentumEvolve(F, k):
+            return F*exp(-0.5 * k*k * dt * 1j)
+
+        self._f = map(spatialEvolve, self._f, self._x)
+        self._F = fft(self._f)
+        self._F = map(momentumEvolve, self._F, self._k)
+        self._f = ifft(self._F)
+        self._f = map(spatialEvolve, self._f, self._x)
+
+    def f(self):
+        return self._f
+
+    def F(self):
+        return self._F
+
+    def x(self):
+        return self._x
+
+    def V(self):
+        return self._V(self._x)
+
+    def setPotential(self, V):
+        self._V = V
+
+if __name__ == '__main__':
+    from matplotlib.pyplot import plot, show
+
+    s = Schroedinger1D(128)
+    plot(s.x(), abs(s.f()))
+
+    for i in xrange(100):
+        s.evolve()
+
+    plot(s.x(), abs(s.f()))
+    show()
